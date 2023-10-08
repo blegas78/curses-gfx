3a046716bfc81ae9df0d4eecb2b15baf92d80171
blegas78 <12centdwarf@gmail.com>
2023-10-08 18:29:14 -0500

    Added support for diffuse color laoding from Assimp, but unsure of the reach of support

--- src/fileLoader.cpp
+++ src/fileLoader.cpp
@@ -59,6 +59,7 @@ typedef struct _LightParamsAndTexture {
     LightParams* lightParams;
     Texture* texture;
     Coordinates3D cameraLocation;
+    Coordinates3D colorDiffuse;
 } LightParamsAndTexture;
 
 void lightModelFs(const FragmentInfo& fInfo) {
@@ -114,9 +115,9 @@ void materialFs(const FragmentInfo& fInfo) {
     double magnitude = normal.z;
     magnitude *= magnitude;
     magnitude = fmin(1,magnitude);
-    fInfo.colorOutput->r = magnitude*255;
-    fInfo.colorOutput->g = magnitude*255;
-    fInfo.colorOutput->b = magnitude*255;
+    fInfo.colorOutput->r = magnitude*255*lpt->colorDiffuse.x;
+    fInfo.colorOutput->g = magnitude*255*lpt->colorDiffuse.y;
+    fInfo.colorOutput->b = magnitude*255*lpt->colorDiffuse.z;
     fInfo.colorOutput->a = 0;
 }
 
@@ -277,7 +278,8 @@ int main(int argc, char** argv) {
         
         printf(" - mesh[%d] has normals:   %d\n", m, scene->mMeshes[m]->HasNormals());
         printf(" - mesh[%d] has faces  :   %d\n", m, scene->mMeshes[m]->mNumFaces);
-        printf(" - mesh[%d][0] has texcoords: %d\n", m, scene->mMeshes[m]->HasTextureCoords(0));
+        printf(" - mesh[%d] has texcoords(0): %d\n", m, scene->mMeshes[m]->HasTextureCoords(0));
+        printf(" - mesh[%d] has colors(0): %d\n", m, scene->mMeshes[m]->HasVertexColors(0));
         //        printf(" - texcooords[0][0] name: %s\n", scene->mMeshes[0]->mTextureCoordsNames[0][0].C_Str());
         
         printf(" - - Allocated!\n");
@@ -314,30 +316,6 @@ int main(int argc, char** argv) {
             }
         }
         
-//        for(int i = 0; i < scene->mMeshes[m]->mNumVertices; i++) {
-//
-//            meshes[m].vi[i].vertex.x = scene->mMeshes[m]->mVertices[i].x;
-//            meshes[m].vi[i].vertex.y = scene->mMeshes[m]->mVertices[i].y;
-//            meshes[m].vi[i].vertex.z = scene->mMeshes[m]->mVertices[i].z;
-//            meshes[m].vi[i].vertex.w = 1;
-//
-//            meshes[m].vi[i].normal.x = scene->mMeshes[m]->mNormals[i].x;
-//            meshes[m].vi[i].normal.y = scene->mMeshes[m]->mNormals[i].y;
-//            meshes[m].vi[i].normal.z = scene->mMeshes[m]->mNormals[i].z;
-//
-//            if(scene->mMeshes[m]->HasTextureCoords(0)) {
-//                meshes[m].vi[i].textureCoord.x = scene->mMeshes[m]->mTextureCoords[0][i].x;
-//                meshes[m].vi[i].textureCoord.y = scene->mMeshes[m]->mTextureCoords[0][i].y;
-//            }
-////            else if(scene->mMeshes[m]->HasTextureCoords(1)) {
-////                meshes[m].vi[i].textureCoord.x = scene->mMeshes[m]->mTextureCoords[1][i].x;
-////                meshes[m].vi[i].textureCoord.y = scene->mMeshes[m]->mTextureCoords[1][i].y;
-////
-////            }
-////            meshes[m].vi[i].materialIndex = scene->mMeshes[m]->mMaterialIndex;
-////            printf("%f %f\n", meshes[m].vi[i].textureCoord.x, meshes[m].vi[i].textureCoord.y);
-//        }
-        
         
         meshes[m].materialId = scene->mMeshes[m]->mMaterialIndex;
         printf("meshes[%d].materialId = %d\n", m, meshes[m].materialId);
@@ -346,6 +324,8 @@ int main(int argc, char** argv) {
     struct Material {
         Texture* texture;
         int numTextures;
+        bool hasDiffuseColor;
+        Coordinates3D colorDiffuse;
     };
     int numMaterials = scene->mNumMaterials;
     Material mMaterials[numMaterials];
@@ -356,14 +336,33 @@ int main(int argc, char** argv) {
 //        printf(" - Name : %s\n", scene->mMaterials[i]->GetName().C_Str());
         printf(" - Material : %d num props:%d\n", i, material->mNumProperties);
 
-        
+        aiPropertyTypeInfo pdas;
         for(int p = 0; p < material->mNumProperties; p++) {
-//            printf(" - - Type: %d\n", scene->mMaterials[i]->mProperties[p]->mType);
-//            printf(" - - Name: %s\n", scene->mMaterials[i]->mProperties[p]->mKey.C_Str());
-//            printf(" - - Semantic: %d\n", scene->mMaterials[i]->mProperties[p]->mSemantic);
-//            printf(" - - Data Length: %d\n", scene->mMaterials[i]->mProperties[p]->mDataLength);
-//            printf(" - - mIndex %d\n", scene->mMaterials[i]->mProperties[p]->mIndex);
+//            printf(" - - Type: %d\n", material->mProperties[p]->mType);
+//            printf(" - - Name: %s\n", material->mProperties[p]->mKey.C_Str());
+//            printf(" - - Semantic: %d\n", material->mProperties[p]->mSemantic);
+//            printf(" - - Data Length: %d\n", material->mProperties[p]->mDataLength);
+//            printf(" - - mIndex %d\n", material->mProperties[p]->mIndex);
+            
+//            ai_real fArray[
+//            switch (material->mProperties[p]->mType) {
+//                case aiPropertyTypeInfo::aiPTI_Float:
+//                    aiGetMaterialFloatArray(material, AI_MATKEY_COLOR_DIFFUSE, material->mProperties[p]->mType, )
+//                    break;
+//
+//                default:
+//                    break;
+//            }
         }
+        
+        aiColor4D diffuse;
+        float c[4];
+        if(AI_SUCCESS == aiGetMaterialColor(material, AI_MATKEY_COLOR_DIFFUSE, &diffuse)) {
+            printf(" - - - Diffuse color: (%f,%f,%f,%f)\n", diffuse.r, diffuse.g, diffuse.b, diffuse.a);
+            mMaterials[i].hasDiffuseColor = true;
+            mMaterials[i].colorDiffuse = {diffuse.r, diffuse.g, diffuse.b};
+        }
+        
         printf(" - Texture count : %d\n", material->GetTextureCount(aiTextureType_DIFFUSE));
 //        for(int t = 0; t < scene->mMaterials[i]->GetTextureCount(aiTextureType_DIFFUSE); t++) {
 //            scene->mMaterials[i]->
@@ -471,6 +470,7 @@ int main(int argc, char** argv) {
         now = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double, std::milli> float_ms = (now - before);
         before = now;
+        before2 = now;
         double dTime = float_ms.count()/1000.0;
         
         delayTime += 0.001*(1.0/60.0 - dTime);
@@ -558,7 +558,7 @@ int main(int argc, char** argv) {
         mLightParamsAndTexture.cameraLocation = {0,0,0};
         
         now = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double, std::milli> float_ms2 = (now - before);
+        std::chrono::duration<double, std::milli> float_ms2 = (now - before2);
         double timeToPrepare = (double)float_ms2.count()/1000.0;
         before2 = now;
         
@@ -580,6 +580,11 @@ int main(int argc, char** argv) {
                 //            mRenderPipeline.setFragmentShader(textureFs);
                 mRenderStats2 = mRenderPipeline.rasterizeShader(meshes[m].vi, &mUniformData, meshes[m].numTriangles, &mLightParamsAndTexture, myVertexShader);
             } else {
+                if(mMaterials[meshes[m].materialId].hasDiffuseColor) {
+                    mLightParamsAndTexture.colorDiffuse = mMaterials[meshes[m].materialId].colorDiffuse;
+                } else {
+                    mLightParamsAndTexture.colorDiffuse = {1,1,1};
+                }
                 
                 mRenderPipeline.setFragmentShader(materialFs);
                 mRenderStats2 = mRenderPipeline.rasterizeShader(meshes[m].vi, &mUniformData, meshes[m].numTriangles, &mLightParamsAndTexture, myVertexShader);
